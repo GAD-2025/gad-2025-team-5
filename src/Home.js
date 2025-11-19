3a2646799d0fc2944f940804827569e0719d8d64
김한솔 <kimhansol@gimhansol-ui-MacBookAir.local>
2025-11-20 02:26:58 +0900

    feat: 커뮤니티 페이지 추가 및 App, Home 수정

--- src/Home.js
+++ src/Home.js
@@ -221,7 +221,7 @@ const Home = () => {
                     <section className="community-posts">
                         <div className="section-header">
                             <h2>커뮤니티 인기글</h2>
-                            <Link to="#" className="view-all">더보기</Link>
+                            <Link to="/community" className="view-all">더보기</Link>
                         </div>
                         <div className="post-list">
                             {/* Post items can be mapped here if they become dynamic */}
@@ -282,10 +282,10 @@ const Home = () => {
                         <i className="fa-solid fa-house"></i>
                         <span>홈</span>
                     </div>
-                    <div className="nav-item">
+                    <Link to="/community" className="nav-item">
                         <i className="fa-regular fa-user-group"></i>
                         <span>커뮤니티</span>
-                    </div>
+                    </Link>
                     <Link to="/register" className="nav-item">
                         <i className="fa-regular fa-square-plus"></i>
                         <span>등록</span>
