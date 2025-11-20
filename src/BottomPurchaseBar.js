c40c022a3e6fa07afb98b1e0b5e58082204077b7
김한솔 <kimhansol@gimhansol-ui-MacBookAir.local>
2025-11-20 13:32:30 +0900

    fix conflict

--- src/BottomPurchaseBar.js
+++ src/BottomPurchaseBar.js
@@ -2,19 +2,11 @@ import React from 'react';
 import { useNavigate } from 'react-router-dom';
 import './BottomPurchaseBar.css';
 
-<<<<<<< Updated upstream
 const BottomPurchaseBar = ({ bookTitle }) => {
   const navigate = useNavigate();
 
   const handleChatClick = () => {
     navigate(`/chat/${bookTitle}`);
-=======
-const BottomPurchaseBar = ({ book }) => {
-  const navigate = useNavigate();
-
-  const handlePurchaseClick = () => {
-    navigate('/payment', { state: { book: book } });
->>>>>>> Stashed changes
   };
 
   return (
